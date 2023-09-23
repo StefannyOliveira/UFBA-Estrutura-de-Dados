94e660a42ee6f25dcb3534e1431868b96e2415dd
StefannyOliveira <130118211+StefannyOliveira@users.noreply.github.com>
2023-09-22 23:36:20 -0300

    Update Sistemas Operacionais.py

--- Sistemas Operacionais.py
+++ Sistemas Operacionais.py
@@ -1,3 +1,14 @@
+#Prática 01.03 - Sistemas Operacionais
+#Você deve implementar o algoritmo Round Robin (RR) de escalonamento em um Sistema Operacional (SO). Esse algoritmo executa da seguinte maneira:
+#  1- Cada processo no sistema tem um custo de execução representado por Unidades de Processamento (UP);
+#  2 - Cada novo processo é enviado para uma fila de "pronto" que contém processos que estão aptos para serem executados;
+#  3 - A CPU libera um limite de tempo (quantum) para cada processo. 
+#Se um processo está executando e precisa de mais UP que o quantum liberado pela CPU, ele executa até o limite e depois volta para o final da fila de pronto com as UPs necessárias para concluir sua tarefa.
+#A primeira linha informa quantas linhas em sequência devem ser lidas. 
+#A segunda linha contém duas informações importantes: o quantum da CPU e o máximo de UP que seu código vai processar. 
+#A próxima linha contém os IDs dos processos. A última linha contém todas as UPs necessárias por processo.
+#Ao final do máximo de UP, seu código deve imprimir a fila atual e as respectivas UPs restantes.
+
 
 class No:
   def __init__(self, valor):
