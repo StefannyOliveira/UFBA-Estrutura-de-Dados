06bcdba5dc26dd6c6b1bc03007557e634deafc68
StefannyOliveira <130118211+StefannyOliveira@users.noreply.github.com>
2023-09-22 23:38:27 -0300

    Update Árvore Binária (Remoção).py

--- Árvore Binária (Remoção).py
+++ Árvore Binária (Remoção).py
@@ -1,3 +1,18 @@
+#Prática 02.02 - Árvore Binária (remoção)
+#1 -Implementar a estrutura de dados árvore binária incluindo, minimamente, as funções listadas abaixo:
+#  Add (INT) - Adiciona um inteiro na árvore;
+#  Remove (INT) - Remove um inteiro da árvore;
+#2 - Ler um vetor de inteiros e armazená-lo em uma árvore binária:
+#  Os valores devem ser armazenados com "Add(...)";
+#  Os elementos devem ser preenchidos da seguinte maneira: se o valor da chave for menor que a raiz, insira na subárvore à esquerda; caso contrário, insira à direita;
+#3 - Ler um novo vetor com os valores que serão removidos:
+#  A remoção deve acontecer usando o algoritmo explicado em sala de aula;
+#  A remoção não deve acontecer nesse momento da leitura dos dados;
+#4 - Ler um valor que será usado como chave de busca:
+#  Execute a função "em-ordem" e imprima os valores encontrados até o número informado para pesquisa;
+#  Execute as remoções listadas na Etapa 2;
+#  Execute a função "em-ordem" e imprima os valores encontrados até o número informado para pesquisa;
+#  Se o número não for encontrado, seu código deve imprimir todos os elementos "em-ordem";
 
 class No:
   def __init__(self, valor):
